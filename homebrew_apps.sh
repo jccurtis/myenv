34eb7082d9f2cfd934e053f8f1da959b830d5fb0
Joseph Curtis <joseph.c.curtis@gmail.com>
2017-02-02 11:19:01 -0800

    Added homebrew install and brew installations

---
+++ homebrew_apps.sh
@@ -0,0 +1,73 @@
+#!/bin/env/bash
+
+# -------------------------------------
+# Install Tools
+# -------------------------------------
+
+tools=(
+  autoconf
+  automake
+  ack
+  cmake
+  curl
+  git
+  mas
+  openssl
+  psgrep
+  shellcheck
+  ssh-copy-id
+  wget
+)
+
+echo "installing tools..."
+brew install ${tools[@]}
+
+# -------------------------------------
+# Install apps to: /Applications
+# Default is:      $HOME/Applications
+# -------------------------------------
+
+apps=(
+  appcleaner
+  atom
+  filezilla
+  firefox
+  flux
+  github-desktop
+  google-chrome
+  #google-hangouts
+  iterm2
+  odrive
+  pdfexpert
+  plain-clip
+  skype
+  spotify
+  sublime-text
+  vlc
+  xquartz
+)
+
+echo "Installing Apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# -------------------------------------
+# Install Fonts
+# -------------------------------------
+
+fonts=(
+  font-monoid
+  font-source-code-pro
+  font-source-code-pro-for-powerline
+  font-open-sans
+)
+
+echo "Installing Fonts..."
+brew cask install ${fonts[@]}#!/bin/sh
+
+
+# -------------------------------------
+# Cleanup
+# -------------------------------------
+
+echo "Cleaning up cask downloads..."
+brew cask cleanup
---
+++ homebrew_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# -------------------------------------
+# Install Homebrew
+# -------------------------------------
+
+echo “Installing Homebrew...”
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# -------------------------------------
+# Tapping cask versions
+# -------------------------------------
+
+brew tap caskroom/versions
+brew tap caskroom/fonts
+brew tap homebrew/dupes
