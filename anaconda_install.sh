584cf3d589cd1ca0c4a8c920d4d40dd940f27db4
Joseph Curtis <joseph.c.curtis@gmail.com>
2017-03-23 23:49:05 -0700

    Added python packages
    flake8, seaborn, future, lmfit, progressbar2

--- anaconda_install.sh
+++ anaconda_install.sh
@@ -4,5 +4,10 @@ wget https://repo.continuum.io/archive/Anaconda3-4.3.0-MacOSX-x86_64.sh -O ~/ana
 bash ~/anaconda_tmp.sh -b -p $HOME/anaconda
 rm ~/anaconda_tmp.sh
 
-printf "Appending to ~/.bash_profile\n"
+printf "Appending to ~/.bash_profile ...\n"
 printf "\n# Activate anaconda\nexport PATH=\"\$HOME/anaconda/bin:\$PATH\"\n" >> ~/.bash_profile
+
+
+printf "Installing python packages ...\n"
+conda install flake8 seaborn future
+pip install lmfit progressbar2
