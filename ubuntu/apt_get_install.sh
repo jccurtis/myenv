bb770ff68b79e92f784b3f8102217438022142bb
Joseph Curtis <jccurtis@lbl.gov>
2017-08-30 16:41:03 -0700

    Added ubuntu installs

---
+++ ubuntu/apt_get_install.sh
@@ -0,0 +1,17 @@
+# General tools
+sudo apt-get install git wget cmake make
+
+# Fonts
+sudo apt-get install fonts-inconsolata
+sudo fc-cache -fv
+
+# Atom
+# http://tipsonubuntu.com/2016/08/05/install-atom-text-editor-ubuntu-16-04/
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install atom
+
+
+
+
+
