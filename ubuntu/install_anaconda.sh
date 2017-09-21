f7d4d0522a0f01bf7e3ff89c2557f9e9e754f300
Joseph Curtis <jccurtis@lbl.gov>
2017-09-21 16:11:42 -0700

    Updated ubuntu anaconda install

--- ubuntu/install_anaconda.sh
+++ ubuntu/install_anaconda.sh
@@ -5,8 +5,11 @@ wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh -O anacon
 bash ~/anaconda.sh -b -p $HOME/anaconda
 rm ~/anaconda.sh
 
-printf "Appending to ~/.bash_profile ...\n"
-printf "\n# Activate anaconda\nexport PATH=\"\$HOME/anaconda/bin:\$PATH\"\n" >> ~/.bash_profile
+printf "Appending to ~/.bashrc ...\n"
+printf "\n# Activate anaconda\nexport PATH=\"\$HOME/anaconda/bin:\$PATH\"\n" >> ~/.bashrc
+
+printf "Activating conda ...\n"
+export PATH="$HOME/anaconda/bin:$PATH"
 
 printf "Installing python packages ...\n"
 conda install flake8 seaborn future
