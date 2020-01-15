b4337939e056192899d3a157065fccd6f7130a60
Joseph Curtis <jccurtis@lbl.gov>
2020-01-15 12:10:08 -0800

    Fix git cred timeout to global

--- general/configure_git.sh
+++ general/configure_git.sh
@@ -3,6 +3,6 @@
 git config --global user.email "jccurtis@lbl.gov"
 git config --global user.name "Joseph Curtis"
 # Change git to cache credentials for 24 hours
-git config credential.helper 'cache --timeout=86400'
+git config --global credential.helper 'cache --timeout=86400'
 # Update git behavior to modern
 git config --global push.default simple
