2a34651f79be62e7d61b51b6adfa7f6b3a979363
Joseph Curtis <jccurtis@lbl.gov>
2020-09-07 23:20:39 -0700

    Only chown texlive if exists

--- OS/macOS/scripts/config_homebrew.sh
+++ OS/macOS/scripts/config_homebrew.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+this_dir="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $this_dir
+
 brew analytics off
 brew update
 brew bundle --verbose
@@ -13,4 +16,6 @@ chmod go-w '/usr/local/share'
 brew cleanup
 
 # Change texlive installation owner so tlmgr will work
-sudo chown -R $(whoami) /usr/local/texlive/
+if [[ -d /usr/local/texlive/ ]]; then
+    sudo chown -R $(whoami) /usr/local/texlive/
+fi
