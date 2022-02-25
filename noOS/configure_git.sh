7ab3f11f0886dc5c3cee494113a2fa66aa917c58
Joey Curtis <joseph.c.curtis@gmail.com>
2022-02-25 11:07:37 -0800

    Add default pull to readme

--- noOS/configure_git.sh
+++ noOS/configure_git.sh
@@ -6,3 +6,4 @@ git config --global user.name "Joseph Curtis"
 git config --global credential.helper 'cache --timeout=86400'
 # Update git behavior to modern
 git config --global push.default simple
+git config --global pull.rebase false
