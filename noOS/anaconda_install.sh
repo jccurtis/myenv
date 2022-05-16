6aef4a5a2bb7ca07bed246a226d044327eb8d490
Joey Curtis <joseph.c.curtis@gmail.com>
2022-05-16 10:29:13 -0700

    Cleanup anaconda install for error handling

--- noOS/anaconda_install.sh
+++ noOS/anaconda_install.sh
@@ -1,32 +1,37 @@
 # Use the current shell
 
-set +e
-set +u
+set -e
+set -u
+
+_script=
 
 if [[ "$(uname)" == 'Darwin' ]]; then
     if [[ `uname -m` == 'arm64' ]]; then
-        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh --output ~/_conda_tmp.sh
+        _script="https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh"
     else
-        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh --output ~/_conda_tmp.sh
+        _script="https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
     fi
 elif [[ "$(uname)" == 'Linux' ]]; then
     if [[ `uname -m` == 'arm64' ]]; then
-        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh --output ~/_conda_tmp.sh
+        _script="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh"
     else
-        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --output ~/_conda_tmp.sh
+        _script="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
     fi
-else
-    echo "Cannot install miniconda on: $(uname)"
+fi
+if [ -z "$_script" ]; then
+    echo "Cannot install miniconda on: $(uname -a)"
     exit 1
 fi
+echo "Downloading: $_script"
+curl "$_script" --output $HOME/_conda_tmp.sh
 
-bash ~/_conda_tmp.sh -b -p $HOME/miniconda
-rm ~/_conda_tmp.sh
+bash $HOME/_conda_tmp.sh -b -p $HOME/miniconda
+rm $HOME/_conda_tmp.sh
 
 if [[ $SHELL == *zsh ]]; then
-    ~/miniconda/bin/conda init zsh
+    $HOME/miniconda/bin/conda init zsh
 elif [[ $SHELL == *bash ]]; then
-    ~/miniconda/bin/conda init bash
+    $HOME/miniconda/bin/conda init bash
 else
     echo "Cannot init anaconda for this shell: $SHELL"
 fi
