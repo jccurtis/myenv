d0fd54fe37c499bccfab6602ded9a3f29cd2723f
Joey Curtis <joseph.c.curtis@gmail.com>
2022-05-15 20:02:13 -0700

    Update anaconda install to use curl and exist if OS doesn't match spec

--- noOS/anaconda_install.sh
+++ noOS/anaconda_install.sh
@@ -5,16 +5,19 @@ set +u
 
 if [[ "$(uname)" == 'Darwin' ]]; then
     if [[ `uname -m` == 'arm64' ]]; then
-        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -O ~/_conda_tmp.sh
+        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh --output ~/_conda_tmp.sh
     else
-        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/_conda_tmp.sh
+        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh --output ~/_conda_tmp.sh
     fi
 elif [[ "$(uname)" == 'Linux' ]]; then
     if [[ `uname -m` == 'arm64' ]]; then
-        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O ~/_conda_tmp.sh
+        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh --output ~/_conda_tmp.sh
     else
-        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/_conda_tmp.sh
+        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --output ~/_conda_tmp.sh
     fi
+else
+    echo "Cannot install miniconda on: $(uname)"
+    exit 1
 fi
 
 bash ~/_conda_tmp.sh -b -p $HOME/miniconda
